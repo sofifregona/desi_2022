fe87496e33d8e30d29f5deff57b7e66d75e60d97
natalia mordini <nati_mordini@hotmail.com>
2022-12-30 08:42:06 -0300

    Historia 1 (clima actual)

--- src/main/java/com/tsti/smn/capaPresentacion/seguridad/SecurityConfig.java
+++ src/main/java/com/tsti/smn/capaPresentacion/seguridad/SecurityConfig.java
@@ -38,7 +38,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     	http.authorizeRequests()                                                          
 //            .antMatchers("/resources/**", "/signup", "/about").permitAll()            
 //            .antMatchers("/personasEditar").hasAuthority("EDIT")                                
-//            .antMatchers("/db/**/**").access("hasRole('ADMIN') and hasRole('DBA')")      
+//            .antMatchers("/db/**/**").access("hasRole('ADMIN') and hasRole('DBA')") 
+        	.antMatchers("/ciudadEditarClima").hasAuthority("ADMIN")      
             .anyRequest().authenticated()                                         
             .and()
         .formLogin();
